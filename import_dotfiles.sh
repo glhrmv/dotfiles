0d4570c69f0c0d66f18e9b90aceefbff4e1f0c3a
glhrmv <gvmgui@gmail.com>
2018-12-13 15:23:43 +0000

    Update import_dotfiles.sh

--- import_dotfiles.sh
+++ import_dotfiles.sh
@@ -1,11 +1,39 @@
 #!/bin/bash
 
+# (bash) .bashrc
+BASHRC_SRC=~/.bashrc
+BASHRC_DST=./bash/.bashrc
+
+# (bash) .bash_profile
+BASHPROFILE_SRC=~/.bash_profile
+BASHPROFILE_DST=./bash/.bash_profile
+
+# (vim) .vimrc
+VIMRC_SRC=~/.vimrc
+VIMRC_DST=./vim/.vimrc
+
+# (neovim) init.vim
+INITVIM_SRC=~/.config/nvim/init.vim
+INITVIM_DST=./neovim/init.vim
+
+# (tmux) .tmux.conf
+TMUXCONF_SRC=~/.tmux.conf
+TMUXCONF_DST=./tmux/.tmux.conf
+
+# (git) .gitconfig
+GITCONFIG_SRC=~/.gitconfig
+GITCONFIG_DST=./git/.gitconfig
+
 echo "[Ø] Updating dotfiles..."
-cp ~/.bashrc bash/.bashrc
-cp ~/.bash_profile bash/.bash_profile
-cp ~/.vimrc vim/.vimrc
-cp ~/.config/nvim/init.vim neovim/init.vim
-cp ~/.tmux.conf tmux/.tmux.conf
-cp ~/.gitconfig git/.gitconfig
+
+cp $BASHRC_SRC $BASHRC_DST
+cp $BASHPROFILE_SRC $BASHPROFILE_DST
+cp $VIMRC_SRC $VIMRC_DST
+cp $INITVIM_SRC $INITVIM_DST
+cp $TMUXCONF_SRC $TMUXCONF_DST
+cp $GITCONFIG_SRC $GITCONFIG_DST
+
 echo "[Ø] Done."
+
 exit 0
+
