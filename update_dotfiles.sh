319d8d8d4704e500cbf7bcdd3597f04b6158a0c5
Guilherme Vale <oguilhermevale@gmail.com>
2020-05-05 12:58:02 +0100

    Update dotfiles

--- update_dotfiles.sh
+++ update_dotfiles.sh
@@ -1,5 +1,13 @@
 #!/bin/zsh
 
+# (zsh) .zshrc
+ZSHRC_SRC=~/.zshrc
+ZSHRC_DST=./zsh/.zshrc
+
+# (zsh) .shell_aliases
+SHELL_ALIASES_SRC=~/.shell_aliases
+SHELL_ALIASES_DST=./.shell_aliases
+
 # (neovim) init.vim
 INITVIM_SRC=~/.config/nvim/init.vim
 INITVIM_DST=./neovim/init.vim
@@ -14,6 +22,10 @@ GITIGNOREGLOBAL_DST=./git/.gitignore_global
 
 echo "[Ø] Updating dotfiles..."
 
+echo "[Ø] (zsh)   Updating .zshrc"
+cp $ZSHRC_SRC $ZSHRC_DST
+echo "[Ø] (zsh)   Updating .shell_aliases"
+cp $SHELL_ALIASES_SRC $SHELL_ALIASES_DST
 echo "[Ø] (nvim)  Updating .initvim"
 cp $INITVIM_SRC $INITVIM_DST
 echo "[Ø] (git)   Updating .gitconfig"
